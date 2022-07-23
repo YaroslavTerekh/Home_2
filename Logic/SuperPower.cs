d68b20255bbbbcaa912f513fd70e2d27db098934
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-07-23 18:48:39 +0300

    add interface

--- Logic/SuperPower.cs
+++ Logic/SuperPower.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExoftCamp_2_Game_.Soldiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace ExoftCamp_2_Game_.Logic
 {
-    interface SuperPower
+    public interface SuperPower
     {
+        public void AddSuperPower(AbstractCharacter character, int power);
     }
 }
