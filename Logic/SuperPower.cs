9b8438aa364d61ab008b925904531576aab06e0c
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-07-23 19:34:06 +0300

    Revert "add interface"
    This reverts commit d68b20255bbbbcaa912f513fd70e2d27db098934.

--- Logic/SuperPower.cs
+++ Logic/SuperPower.cs
@@ -1,5 +1,4 @@
-﻿using ExoftCamp_2_Game_.Soldiers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace ExoftCamp_2_Game_.Logic
 {
-    public interface SuperPower
+    interface SuperPower
     {
-        public void AddSuperPower(AbstractCharacter character, int power);
     }
 }
