e54d63b95e7bf404e0691e9d26ec25a726a2d452
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-07-23 18:28:57 +0300

    Create SuperPowerGenerator class

---
+++ SuperPowerGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoftCamp_2_Game_
+{
+    public class SuperPowerGenerator
+    {
+        public void GetSuperPower()
+        {
+            int time = DateTime.Now.Millisecond;
+            int result = (time % 6) + 5;
+            //int result = (time % 6) - 5 + 10 / 2 * 2;  // - with all operators
+        }
+    }
+}
